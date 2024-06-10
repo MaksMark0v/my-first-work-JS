d04d503ecb2d4dd40a8a4be4ec6fc04a4794e954
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-06-10 14:45:52 +0300

    Update arr-obj-tasks.js

--- arr-obj-tasks/arr-obj-tasks.js
+++ arr-obj-tasks/arr-obj-tasks.js
@@ -1,18 +1,18 @@
 const arrind = [
-    "Зберігати дані", 
-    "Гуртувати дані", 
-    "Сортувати дані", 
-    "Швидко додавати або видаляти данні", 
-    "Пошук елементів с заданими значеннями", 
-    "Сумування елементів масиву", 
+    "Зберігати дані",
+    "Гуртувати дані",
+    "Сортувати дані",
+    "Швидко додавати або видаляти данні",
+    "Пошук елементів с заданими значеннями",
+    "Сумування елементів масиву",
     "Структуроване зберігання даних"
-    ];
+];
 console.log(arrind);
 
 const book1 = {
-    title : "Da Vinci Code",
-    pageCount : "519",
-    genre : "detective novel"
+    title: "Da Vinci Code",
+    pageCount: "519",
+    genre: "detective novel"
 };
 console.log(book1);
 
@@ -20,10 +20,27 @@ console.log(book1);
 
 book1.authors = [
     {
-    name : "Dan Brown",
-    age : "59"
-}
+        name: "Dan Brown",
+        age: "59"
+    }
 ];
 console.log(book1.authors);
 
 
+
+// Створюємо масив з 10 користувачів
+const users = [];
+
+for(let i = 1; i <= 10; i++) {
+    const user = {
+        name: `Ім'я ${i}`,
+        username: `Користувач ${i}`,
+        password: `Пароль ${i}`
+    };
+    users.push(user);
+}
+
+// Виводимо в консоль пароль кожного користувача
+users.forEach(user => {
+    console.log(`Пароль користувача ${user.username}: ${user.password}`);
+});
