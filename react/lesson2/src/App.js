e72f0863336a51030d3113b615024a521e90bc17
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-07-31 20:58:37 +0300

    i

---
+++ react/lesson2/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Container from 'react-boodstrap/Container';
+
+function App () {
+    return (
+        <Container>
+            <div className = 'App'>
+            <h1>Hello, React!</h1>
+            </div>
+        </Container>
+    );    
+}
+
+export default App;
