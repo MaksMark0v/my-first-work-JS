d265c323de49d31afcd7e515a004ede06b880e94
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-08-05 20:39:40 +0300

    up

---
+++ react/lesson3/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+    return (
+        <div>
+            <h1>Привіт!</h1>
+            <p>Це мій компонент у React.</p>
+        </div>
+    );
+};
+
+export default Greeting;
