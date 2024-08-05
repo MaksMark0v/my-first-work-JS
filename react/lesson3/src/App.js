0cc6a986c468e7bb7542b0c5c6332f1aa88e852d
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-08-05 22:22:22 +0300

    Update App.js

--- react/lesson3/src/App.js
+++ react/lesson3/src/App.js
@@ -5,7 +5,7 @@ function App () {
     return (
         <Container>
             <div className = 'App container'>
-            <h1 className="text-center bg-info text-white p-3 mt-3">Привіт!</h1>
+            <h1 className="text-center bg-danger text-white p-3 mt-3">Привіт!</h1>
             </div>
         </Container>
     );    
