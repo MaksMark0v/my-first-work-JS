30ceb7460f6bd9f5a0245dbdb69d14f9f321a36d
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-08-26 11:08:39 +0300

    router

---
+++ react/my-app/src/views/NASAImageDetails/NASAImafeDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NASAImageDetails = () => {
+    return (
+        <div>
+            <h2>NASA Image Details</h2>
+        </div>
+    );
+};
+
+export default NASAImageDetails;
